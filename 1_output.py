9e8c3e18eb6d2f3fd9d7b1c3c733a4479ba863ed
simon <simonkim9883@gmail.com>
2022-10-26 22:30:49 +0300

    first commit

---
+++ 1_output.py
@@ -0,0 +1,6 @@
+# Output in Python
+# The print()function is used to display output
+
+print("Hello World")
+print(2+2)
+print(10 % 3)
---
+++ 2_datatypes.py
@@ -0,0 +1,22 @@
+
+x = 5
+y = 2.5
+
+# pyhton allows variable declarations on the same line
+x, y = 5, 2.5
+a = b = c = 3
+
+my_string = ("Hello There")
+is_true = True
+is_false = False
+my_first_list = [1, 2, 3]
+none_type = None
+
+my_char = ('a')
+
+print(type(x))
+print(type(y))
+print(type(a))
+print(type(string))
+print(type(is_true))
+print(type(my_first_list))
