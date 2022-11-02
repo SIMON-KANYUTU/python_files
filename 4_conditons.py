b5d80e4b5ac1f566b374e3164b16fc2e0aad434f
simon <simonkim9883@gmail.com>
2022-11-02 20:09:41 +0300

    2nd Nov

--- 4_conditons.py
+++ 4_conditons.py
@@ -4,3 +4,19 @@ if (driver_age >= 18):
     print("Eligible to have a driver's license")
 else:
     print("Not Eligible to have a driver's license")
+    print(f"Please check back with us in {18 -driver_age} years")
+
+
+# elif
+student_mark = int(input("Enter Final Mark: "))
+
+if (student_mark >= 70):
+    print("Pass: Grade A")
+elif (student_mark >= 60):
+    print("Pass: Grade B")
+elif (student_mark >= 50):
+    print("Pass: Grade C")
+elif (student_mark >= 40):
+    print("Pass: Grade D")
+else:
+    print("Fail")
---
+++ 5_loops.py
@@ -0,0 +1,11 @@
+# python implements loops using the 'for' keyword
+# Accompanied by some additional logic
+
+my_fruits = ["Mango", "Orange" "Peach" "Apple"]  # list
+for fruit in my_fruits:
+    print(fruit)
+    print(f"The list {my_fruits} has {len(my_fruits)}items")
+
+my_name = "Jonathan"  # string (transverses each character)
+for i in my_name:
+print(i)
